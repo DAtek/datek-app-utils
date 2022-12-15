baf69b73761438e264859ef7a3b256a1037d97a7
Attila Dudas <dudasa7@gmail.com>
2022-12-15 09:36:47 +0100

    Fix wording

--- tests/env_config/test_types.py
+++ tests/env_config/test_types.py
@@ -5,13 +5,13 @@ from datek_app_utils.env_config.types import Bool
 
 class TestBool:
     @mark.parametrize("value", [False, "0", "false", "False", "no", "No"])
-    def test_mandatory_field_false(self, value, monkeypatch):
+    def test_false(self, value, monkeypatch):
         assert Bool(value) is False
 
     @mark.parametrize("value", [True, "1", "true", "True", "yes", "Yes"])
-    def test_mandatory_field_true(self, value, monkeypatch):
+    def test_true(self, value, monkeypatch):
         assert Bool(value) is True
 
-    def test_mandatory_field_invalid(self):
+    def test_invalid(self):
         with raises(ValueError):
             Bool("this is invalid")
